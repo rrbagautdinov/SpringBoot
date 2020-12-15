e916a9cc30e71f672f6a9ba69c5afa9cb2bec878
rrbagautdinov <wh1sperdnb@mail.ru>
2020-12-15 13:36:18 +0300

    fix @Data

--- src/main/java/ru/geekbrains/boot/shop/model/Product.java
+++ src/main/java/ru/geekbrains/boot/shop/model/Product.java
@@ -1,12 +1,8 @@
 package ru.geekbrains.boot.shop.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
